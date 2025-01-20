a76f47de91caf17602f1f3866f4eeec02a86d116
Tien Lam <nhattienlam2005@gmail.com>
2025-01-20 16:30:52 +0700

    shuffle image array using  fisher yate

--- animal_cards_matching/animal_cards_matching/src/App.jsx
+++ animal_cards_matching/animal_cards_matching/src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const [indexImg, setIndexImg] = useState([]);
+  const [selectedSquare, setSelectedSquare] = useState(null);
+  const [squares, setSquares] = useState([]);
+  
+  useEffect(() => {
+    const imgArray = Array.from({length: 100}, (_, index) => Math.floor(index/2));
+    for( let i = imgArray.length - 1; i > 0; i--){
+      const j = Math.floor(Math.random() * (i + 1));
+      [imgArray[i], imgArray[j]] = [imgArray[j], imgArray[i]];
+    }
+    setIndexImg[imgArray];
+    setSquares(Array(100).fill(null));
+  }, []);
 
   return (
     <>
-      
+      {/* Add your JSX content here */}
     </>
   )
 }
